b0bb847ebdad568d0877d077437543232f40c1b3
Brad Oyler <bradoyler@gmail.com>
2016-05-12 01:15:05 -0400

    add a few versions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # v8-flags
+
+Docs for V8 flags by Node version
+
+### To Generate docs
+
+```
+$ git clone git@github.com:bradoyler/v8-flags.git`
+$ cd $_
+```
+
+- Install [nvm](https://github.com/creationix/nvm)
+
+```
+$ nvm use <version>
+$ node index
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+var flags = require('v8flags');
+var fs = require('fs');
+
+function minorVer(version) {
+    return version.split('.').splice(0,2).join('.');
+}
+
+flags(function (err, results) {
+
+    if(err) {
+        return console.log('Err:', err);
+    }
+
+    results.sort(function(a, b){
+        if(a < b) return -1;
+        if(a > b) return 1;
+        return 0;
+    });
+
+    var stream = fs.createWriteStream('v'+ minorVer(process.versions.node)+'.md');
+    stream.once('open', function(fd) {
+
+        stream.write('Versions: Node='+ process.versions.node+ ' V8='+ process.versions.v8 +'\n');
+
+        results.forEach(function(result) {
+            stream.write(result+'\n');
+        });
+        stream.end();
+    });
+
+});
